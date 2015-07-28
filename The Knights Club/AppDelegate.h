1c01cf76c767166a97bf9ab20ba591e1c92d4cbc
chrisyao4700 <chrisyao900908@gmail.com>
2015-07-28 01:49:50 -0400

    Initial Commit

---
+++ The Knights Club/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Knights Club
+//
+//  Created by 姚远 on 7/28/15.
+//  Copyright (c) 2015 Gannon University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Knights Club/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  The Knights Club
+//
+//  Created by 姚远 on 7/28/15.
+//  Copyright (c) 2015 Gannon University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
